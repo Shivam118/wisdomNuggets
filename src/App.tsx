f53e408bf04a61c939e2272482dbe9ae5f627f12
Shivam118 <sharmashushant118@gmail.com>
2024-01-02 22:13:10 +0530

    Product Cards Ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,140 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Content, Footer } = Layout;
+
+// .productCards:hover .viewProduct
+//   display: block
+//   cursor: pointer
+//   position: absolute
+//   bottom: 0
+//   width: 100%
+//   color: #fff
+//   background: #6d84ffcc
+//   padding: 10px 0
+//   text-align: center
+//   transition: 1s ease-in-out
+//   &:hover
+//     text-decoration: underline
+
+interface ProductItem {
+  id: string | number;
+  name: string;
+  price: string | number;
+  image: string;
+  ratings: number;
+  reviews: number;
+  link: string;
 }
+
+const RatingsBar: Array<string> = [
+  "⭐",
+  "⭐ ⭐",
+  "⭐ ⭐ ⭐",
+  "⭐ ⭐ ⭐ ⭐",
+  "⭐ ⭐ ⭐ ⭐ ⭐"
+]
+
+const ProductCards: React.FC<ProductItem> = ({
+  id,
+  name,
+  price,
+  image,
+  ratings,
+  reviews,
+  link
+}) => {
+  return (
+    <div style={{ width: "280px", marginTop: "15px" }}>
+      <div style={{ width: "100%", height: "200px" }}>
+        <img src={image} alt={name} style={{ width: "100%", objectFit: "cover", height: "100%" }} />
+        <div style={{ display: 'none' }}>View Product</div>
+      </div>
+      <div style={{ width: "100%", display: "flex", justifyContent: "center", flexDirection: 'column', gap: "5px" }}>
+        <h5 style={{ fontSize: "14px", textTransform: "capitalize", fontWeight: '500', margin: "10px 0 0 0" }}>{name}</h5>
+        <div style={{ fontSize: "14px", fontWeight: '600', color: "#6d84ff" }}><span style={{ fontSize: "14px", color: "#333", fontWeight: "300", textDecoration: "line-through", marginRight: "5px" }}>Rs.{price}</span>Rs.{price}</div>
+        <div style={{ fontSize: "12px", color: "#333" }}>{`${RatingsBar[ratings - 1]}  (${reviews})`}</div>
+      </div>
+    </div >
+  )
+}
+
+const App: React.FC = () => {
+
+  const items = [
+    {
+      title: "title",
+      image: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/09/instagram-image-size.jpg",
+      price: 100,
+      link: "link"
+    },
+    {
+      title: "title",
+      image: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/09/instagram-image-size.jpg",
+      price: 100,
+      link: "link"
+    },
+    {
+      title: "title",
+      image: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/09/instagram-image-size.jpg",
+      price: 100,
+      link: "link"
+    },
+    {
+      title: "title",
+      image: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/09/instagram-image-size.jpg",
+      price: 100,
+      link: "link"
+    },
+    {
+      title: "title",
+      image: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/09/instagram-image-size.jpg",
+      price: 100,
+      link: "link"
+    },
+    {
+      title: "title",
+      image: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/09/instagram-image-size.jpg",
+      price: 100,
+      link: "link"
+    },
+    {
+      title: "title",
+      image: "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/09/instagram-image-size.jpg",
+      price: 100,
+      link: "link"
+    }
+  ];
+
+  return (
+    <Layout>
+      <Header style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="demo-logo" />
+        <h2 style={{ color: "white" }}>Wisdom Nuggets</h2>
+      </Header>
+      <Content style={{ padding: '0 48px' }}>
+        <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+          <div style={{ display: 'flex', flexWrap: 'wrap', gap: "15px" }}>
+            {items.map((item, index) => (
+              <ProductCards
+                key={index}
+                id={index}
+                name={item.title}
+                image={item.image}
+                price={item.price}
+                link={item.link}
+                ratings={4}
+                reviews={100}
+              />
+            ))}
+          </div>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Amazon Finds ©{new Date().getFullYear()} Owned by Wisdom Nuggets
+      </Footer>
+    </Layout>
+  );
+};
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD68MyoSvxk7CezquvAspo2ZDk_mCqihaQ",
+  authDomain: "wisdomnuggets-ed856.firebaseapp.com",
+  projectId: "wisdomnuggets-ed856",
+  storageBucket: "wisdomnuggets-ed856.appspot.com",
+  messagingSenderId: "157562983213",
+  appId: "1:157562983213:web:f0ba6433d151211f2e79a4",
+  measurementId: "G-6XH693QZFY",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = app.firestore();
+
+export { db };
